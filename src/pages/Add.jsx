bca1e7975ae0d2f37edaa05ba94009555e6fe12f
James Diffley <diffs199@googlemail.com>
2023-10-09 19:57:17 +0100

    NavLinks confirmed

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,8 +1,13 @@
 import React from "react"
+import TempHeader from "../components/TempHeader"
 
 function Add() {
     return (
+        <>
+        <TempHeader/>
         <div>Add</div>
+        </>
+        
     )
 }
 
--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,8 +1,12 @@
 import React from "react"
+import TempHeader from "../components/TempHeader"
 
 function Update() {
     return (
+        <>
+        <TempHeader/>
         <div>Update</div>
+        </>
     )
 }
 
