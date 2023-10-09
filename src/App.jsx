9b0bd2c2d51a23db6f942d38f76f88312b144911
James Diffley <diffs199@googlemail.com>
2023-10-09 21:09:58 +0100

    added MUI and theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+//fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+import { ThemeProvider} from '@mui/material/styles';
+import theme from './theme/theme';
+
+import Layout from './components/Layout';
 
 //import pages
 import List from './pages/List';
@@ -8,12 +19,14 @@ import Add from './pages/Add';
 import Update from './pages/Update';
 import NotFound from './pages/NotFound';
 
-import Layout from './components/Layout';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
 
   return (
     <Router>
+      <CssBaseline/>
+      <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Layout />}>
         <Route index element={<List />} />
@@ -22,6 +35,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
+      </ThemeProvider>
     </Router>
   )
 }
