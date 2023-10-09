8ed7a5046a3dac46b76065cbe418db63660ea20e
James Diffley <diffs199@googlemail.com>
2023-10-09 20:24:20 +0100

    Layout Component Added

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,10 +1,9 @@
 import React from "react"
-import TempHeader from "../components/TempHeader"
+
 
 function List() {
     return (
         <>
-        <TempHeader/>
         <div>List</div>
         </>
     )
