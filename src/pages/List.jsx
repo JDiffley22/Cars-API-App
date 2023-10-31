a0cd7df71e379c340931b554f23be61a5028ed38
James Diffley <diffs1991@googlemail.com>
2023-10-31 16:08:13 +0000

    url corrected end slash

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -12,57 +12,28 @@ import Typography from "@mui/material/Typography";
 
 import { CarsContext } from "../components/contexts/car.context";
 
-function CarsList() {
-const {cars, fetchCars} = useContext(CarsContext)
+import CarsList from "../components/carsList";
+
+function CarsListPage() {
+const {cars, fetchCars, deleteCar} = useContext(CarsContext)
 
 useEffect (()=>{
   fetchCars();
 }, [fetchCars])
 
-  // const cars = [
-  //   {
-  //     _id: 1,
-  //     name: 'Ferrari',
-  //     bhp: 1234,
-  //     avatar_url: "https://www.thecarexpert.co.uk/wp-content/uploads/2019/04/200083-car-ferrari-portofino-m-1920x960.jpeg.webp"
-  //   },
-  //   {
-  //     _id: 2,
-  //     name: 'Bugatti',
-  //     bhp: 1999,
-  //     avatar_url: "https://newsroomcdn.bugatti.com/w_3200/s3-newsroom-bugatti/fba83c23-536b-476f-8fce-3f6e7e8977e1.jpg"
-  //   }
-  // ];
+const deleteHandler = (id) => {
+  deleteCar(id);
+}
 
   return (
     <>
       <Typography variant="h3" component="h2">
         Cars
       </Typography>
-      <List>
-        {cars.map(({ name, bhp, avatar_url, _id }, i) => (
-          <ListItem key={i}>
-            <ListItemAvatar>
-              <Avatar alt="" src={avatar_url} />
-            </ListItemAvatar>
-            <ListItemText>
-              {name} (BHP: {bhp})
-            </ListItemText>
-            <IconButton
-              aria-label="update"
-              to={`/update/${_id}`}
-              component={Link}
-            >
-              <EditIcon />
-            </IconButton>
-            <IconButton aria-label="delete" onClick={() => console.log(`Delete ${_id}`)}>
-              <DeleteIcon />
-            </IconButton>
-          </ListItem>
-        ))}
-      </List>
+     <CarsList cars={cars} deleteHandler={deleteHandler} />
+
     </>
   )
 }
 
-export default CarsList;
+export default CarsListPage;
